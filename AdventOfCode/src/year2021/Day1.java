ad24bf1e0932d011fe8fd330f67660dd0100cbdb
MrsSpindler <Karen.Spindler@ocsb.ca>
2021-11-25 10:30:14 -0500

    Initial commit

---
+++ AdventOfCode/src/year2021/Day1.java
@@ -0,0 +1,15 @@
+package year2021;
+
+public class Day1 {
+
+    // You will need to replace this with the input you are given 
+    // (inputs are unique to every player)
+    // But try it first with the samples in the problem!
+    final static String input = "";
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
